731196fd5228b1eb38533e3b693f458b5e690533
shamshetov2007 <azshamshetov@mai.education>
2026-03-25 15:59:36 +0300

    fixed generate.py, added response model

--- backend/app/routers/generate.py
+++ backend/app/routers/generate.py
@@ -1,18 +1,26 @@
 from fastapi import APIRouter, HTTPException
 from backend.app.services.password_generator import generate_password
-from backend.app.schemas.password import PasswordRequest
-router=APIRouter()
+from backend.app.schemas.password import PasswordRequest, PasswordResponse
+
+router = APIRouter()
 
 
-
-@router.post("/generate")
-async def generate_password(password: PasswordRequest)->dict:
+@router.post("/generate", response_model=PasswordResponse)
+async def generate_password(data: PasswordRequest) -> PasswordRequest:
     try:
-        return {"secret_password": generate_password(password.length, password.use_lower,
-                                                     password.use_upper, password.use_digits,
-                                                     password.use_symbols, 100)}
+        password = generate_password(data.length, data.use_lower,
+                                     data.use_upper, data.use_digits,
+                                     data.use_symbols, 100)
+
+        return PasswordResponse(
+            password=password,
+            length=data.length,
+            used_lower=data.use_lower,
+            used_upper=data.use_upper,
+            used_digits=data.use_digits,
+            used_symbols=data.use_symbols,
+        )
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
