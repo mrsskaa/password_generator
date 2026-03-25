d68a4be0cc151c6c8bb0895703944d53fdf9e7ae
Yurin Matvey <murin8102@gmail.com>
2026-03-25 15:44:15 +0300

    generate update

--- backend/app/routers/generate.py
+++ backend/app/routers/generate.py
@@ -1,22 +1,16 @@
 from fastapi import APIRouter, HTTPException
-from pydantic import BaseModel
 from backend.app.services.password_generator import generate_password
-
+from backend.app.schemas.password import PasswordRequest
 router=APIRouter()
 
-class Password(BaseModel):
-    length : int
-    use_of_lowercase_letters : bool
-    use_of_uppercase_letters : bool
-    use_of_digits : bool
-    use_of_special_symbols : bool
+
 
 @router.post("/generate")
-async def generate_password(password: Password)->dict:
+async def generate_password(password: PasswordRequest)->dict:
     try:
-        return {"secret_password": generate_password(password.length, password.use_of_lowercase_letters,
-                                                     password.use_of_uppercase_letters, password.use_of_digits,
-                                                     password.use_of_special_symbols, 100)}
+        return {"secret_password": generate_password(password.length, password.use_lower,
+                                                     password.use_upper, password.use_digits,
+                                                     password.use_symbols, 100)}
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
     except Exception as e:
